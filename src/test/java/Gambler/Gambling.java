0931cfb1b8fc5841cf6a69695b9f9eaf9a8f0300
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-31 17:58:38 +0530

    lucky day Added

--- src/test/java/Gambler/Gambling.java
+++ src/test/java/Gambler/Gambling.java
@@ -5,6 +5,13 @@ public class Gambling {
     public static final int Stake = 100;
     public static final int BET = 1;
 
+
+    private static int checkWin (){
+        return (int) Math.floor(Math.random() * 10) % 2;
+    }
+
+
+
     public static void main(String[] args) {
         Gambling gambler = new Gambling();
         Scanner scanner = new Scanner(System.in);
@@ -74,7 +81,7 @@ public class Gambling {
         System.out.println("In 20 Days Total Loses" + countLose);
     }
     //UC5
-    
+
     public void monthlyCalculate(int winLimit, int loseLimit) {
         int checkWin = winLimit;
         int checkLose = loseLimit;
@@ -100,7 +107,44 @@ public class Gambling {
         System.out.println(" Won By :- " + wonByHowMuch);
         System.out.println(" Lose By :- " + loseByHowMuch);
     }
+    //UC6
+    public void checkLuckyOrUnluckyDay(int winLimit, int loseLimit) {
+        int checkWin = winLimit;
+        int checkLose = loseLimit;
+        int days = 30;
+        int countWin = 0;
+        int countLose = 0;
+        int day = 0;
+        int luckiestDay = 0;
+        int unLuckiestDay = 0;
+
+        for (int index = 0; index < days; index++, day++) {
+
+            int resultOfDay = gamePLay(winLimit, loseLimit);
+            if (resultOfDay == checkWin) {
+                countWin++;
+                luckiestDay = day;
+                System.out.println("Luckyiest Day " + luckiestDay + "");
+
+            } else if (resultOfDay == checkLose) {
+                countLose++;
+                unLuckiestDay = day;
+                System.out.println(" Unluckyiest Day " + unLuckiestDay);
+            }
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Monthly How Many Luckiest Days And Unluckiest Days");
+        System.out.println("In Month Luckiest Days" + countWin);
+        System.out.println("In Month Unluckiest Days" + countLose);
+    }
 }
+
+
+
+
+
+
 
 
 
