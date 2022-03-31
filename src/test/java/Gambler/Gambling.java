e9c793bd2370ba94cc5d2c8c5000016b6640714d
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-31 09:14:53 +0530

    win or lose

--- src/test/java/Gambler/Gambling.java
+++ src/test/java/Gambler/Gambling.java
@@ -1,10 +1,25 @@
 package Gambler;
-//UC1
+
 public class Gambling {
     public static final int Stake = 100;
     public static final int BET = 1;
 
     public static void main(String[] args) {
+        Gambling gambler = new Gambling();
         System.out.println("Welcome to Gambling Game");
+        gambler.checkWinOrLose();
+        System.out.println("Game Over");
+    }
+        //UC2
+    public void checkWinOrLose() {
+        boolean randomCheck = (Math.random() < 0.5);
+
+        if (randomCheck) {
+            System.out.println(" Congrats Player Win 1$ ");
+        } else {
+            System.out.println(" Player Lose 1$ ");
+
+
+        }
     }
 }
