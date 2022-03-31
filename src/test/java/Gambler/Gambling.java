daf319274e88624beb5a2cf3ede9c9399292a5d7
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-31 09:56:54 +0530

    wining or losing limit added

--- src/test/java/Gambler/Gambling.java
+++ src/test/java/Gambler/Gambling.java
@@ -1,4 +1,5 @@
 package Gambler;
+import java.util.Scanner;
 
 public class Gambling {
     public static final int Stake = 100;
@@ -6,20 +7,42 @@ public class Gambling {
 
     public static void main(String[] args) {
         Gambling gambler = new Gambling();
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Gambling Game");
-        gambler.checkWinOrLose();
-        System.out.println("Game Over");
+        System.out.println("Set Limit till you want to win UC3");
+        int winLimit1 = scanner.nextInt();
+        System.out.println("set limit till you can lose");
+        int loseLimit1 = scanner.nextInt();
+        gambler.gamePLay(winLimit1, loseLimit1);
+        System.out.println();
+        System.out.println("Use Case 3 Finished");
     }
-        //UC2
-    public void checkWinOrLose() {
+
+    //UC2
+    public boolean checkWinOrLose() {
         boolean randomCheck = (Math.random() < 0.5);
 
         if (randomCheck) {
             System.out.println(" Congrats Player Win 1$ ");
         } else {
             System.out.println(" Player Lose 1$ ");
-
-
         }
+        return randomCheck;
+    }
+    //UC3
+
+    public int gamePLay(int winLimit, int loseLimit) {
+        int stake = 100;
+
+        while (stake < winLimit && stake > loseLimit) {
+            if (checkWinOrLose()) {
+                stake = stake + 1;
+                System.out.println(stake + " ");
+            } else {
+                stake = stake - 1;
+                System.out.println(stake + " ");
+            }
+        }
+        return stake;
     }
 }
