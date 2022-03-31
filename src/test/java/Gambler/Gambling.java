6d46c93c9f0e929c87b38314552ebaf665a1ab13
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-31 08:36:40 +0530

    UC1

---
+++ src/test/java/Gambler/Gambling.java
@@ -0,0 +1,10 @@
+package Gambler;
+//UC1
+public class Gambling {
+    public static final int Stake = 100;
+    public static final int BET = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Game");
+    }
+}
